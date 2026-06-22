9bfc84c5405bb5b5c628bd99640425033a7c2e50
suleman-jaam <jaamsulemanmazhar@gmail.com>
2026-06-22 14:11:45 +0500

    fix build

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
